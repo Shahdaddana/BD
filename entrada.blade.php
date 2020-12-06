5af7b1925cf81fa51f71317462301beeba02c048
Shahdaddana <53065949+Shahdaddana@users.noreply.github.com>
2020-12-06 20:18:11 -0300

    Add files via upload

---
+++ entrada.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Musicas</title>
+</head>
+<body>
+<div class="container">
+<div class="box">
+
+<h1>Musicas</h1>
+
+</div>
+</div>
+</body>
+</html>
+
+<style>
+.container {
+width: 100vw;
+height: 100vh;
+background: #6C7A89;
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center
+}
+.box {
+width: 300px;
+height: 200px;
+background: #6C7A89;
+justify-content: center;
+align-items: center
+}
+body {
+margin: 0px;
+}
+.externa {
+align-items: center;
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: center;
+}
+
+</style>
